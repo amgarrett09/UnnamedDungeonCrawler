90e78b4c3dc685c2e525a8dabee74f0c443b8465
Alex Garrett <agarrettR8@gmail.com>
2021-12-17 20:45:36 -0500

    remove unnecessary flag

--- sdl_build.sh
+++ sdl_build.sh
@@ -2,4 +2,4 @@ mkdir -p build/./src/ && \
 clang  -O2 -Wall -Wvla -Wextra -march=native -mavx2 -mfma -fno-strict-aliasing \
 -c src/sdl_main.c \
 -o build/./src/sdl_main.c.o && \
-clang ./build/./src/sdl_main.c.o -o build/sdl_udc -lSDL2 -fno-strict-aliasing
+clang ./build/./src/sdl_main.c.o -o build/sdl_udc -lSDL2
